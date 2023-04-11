0d4815412fb28159a4b6307048ba5fbaa1a9d2cd
SHILPA CHHABRA <chhabrashilpa@yahoo.com>
2023-04-11 14:19:38 +0100

    Copy of Java Fundamentals with Intellij from Makers

---
+++ bites/examples/JavaQuiz.java
@@ -0,0 +1,13 @@
+package bites.examples;
+public class JavaQuiz {
+    public static void main(String[] args) {
+        System.out.println("Q: Does all Java code have to be inside a class?");
+        System.out.println("Ans: Yes");
+        System.out.println("Q: What is compilation?");
+        System.out.println("Ans: The process by which Java code is converted to Byte code prior to execution");
+        System.out.println("Q: What is special about the main method?");
+        System.out.println("Ans: It is executed automatically and you need one per program (no more, no less");
+        System.out.println("Q: What does void mean?");
+        System.out.println("Ans: In the context of a method signature, it means the method is not intended to return anything");
+    }
+}
